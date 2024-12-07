fcb3923022e59063b2564a3457c3009d668d606a
mdenimln <denimlnm@gmail.com>
2024-12-07 11:20:01 +0700

    navigate event  update

--- src/script.js
+++ src/script.js
@@ -4,22 +4,43 @@ let navlinksSpan = document.querySelectorAll("#naviget ul .navilistSpan span");
 console.log(navlinksSpan);
 console.log(navlinks);
 
+window.addEventListener("scroll", () => {
+  let current = "";
 
-window.onscroll = () => {
   sections.forEach((sec) => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
+    const sectionTop = sec.offsetTop - 60; // Sesuaikan tinggi navbar
+    const sectionHeight = sec.offsetHeight;
 
-    if (top >= offset && top < offset + height) {
-      navlinks.forEach((links) => {
-        links.classList.remove("active");
-        document
-          .querySelector("#naviget ul a[href*=" + id + "]")
-          .classList.add("active");
-      });
+    if (
+      window.scrollY >= sectionTop &&
+      window.scrollY < sectionTop + sectionHeight
+    ) {
+      current = sec.getAttribute("id");
     }
   });
+  navlinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
 
-};
+// window.onscroll = () => {
+//   sections.forEach((sec) => {
+//     let top = window.scrollY;
+//     let offset = sec.offsetTop - 150;
+//     let height = sec.offsetHeight;
+//     let id = sec.getAttribute("id");
+
+//     if (top >= offset && top < offset + height) {
+//       navlinks.forEach((links) => {
+//         links.classList.remove("active");
+//         document
+//           .querySelector("#naviget ul a[href*=" + id + "]")
+//           .classList.add("active");
+//       });
+//     }
+//   });
+
+// };
